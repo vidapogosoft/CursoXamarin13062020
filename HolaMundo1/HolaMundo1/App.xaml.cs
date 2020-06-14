0125c4327dc2c0279cab804bbb70219749d6e412
vidapogosoft <vidapogosoft@hotmail.com>
2020-06-13 21:14:45 -0500

    Talleres dia 1
    Practica con
    Hola Mundo
    StackLayout
    GridLayout
    Static Resource - Style Controls

---
+++ HolaMundo1/HolaMundo1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HolaMundo1.Services;
+using HolaMundo1.Views;
+
+namespace HolaMundo1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HolaMundo1/HolaMundo1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HolaMundo1.Models;
+
+namespace HolaMundo1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
