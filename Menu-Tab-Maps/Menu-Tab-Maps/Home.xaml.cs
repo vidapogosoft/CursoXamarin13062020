9e505c4a59df3145ed459bf0c31a4ca8c75b5b28
vidapogosoft <vidapogosoft@hotmail.com>
2020-06-27 15:17:14 -0500

    Collection view
    ejercicio con collection view
    mapas

--- Menu-Tab-Maps/Menu-Tab-Maps/Home.xaml.cs
+++ Menu-Tab-Maps/Menu-Tab-Maps/Home.xaml.cs
@@ -7,14 +7,58 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Menu_Tab_Maps.Model;
+
 namespace Menu_Tab_Maps
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Home : ContentPage
-{
-    public Home()
+    public partial class Home : ContentPage
     {
-        InitializeComponent();
+        public List<Tiendas> Alltiendas;
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Alltiendas = new List<Tiendas>()
+            {
+                new Tiendas
+                {
+                    Name = "Wallmart",
+                    ImageUrl = "http://18.218.178.167/imagesemail/walmart.jpg"
+                },
+
+                new Tiendas
+                {
+                    Name = "Target",
+                    ImageUrl = "http://18.218.178.167/imagesemail/target.jpg"
+                },
+
+                new Tiendas
+                {
+                    Name = "Ferreteri",
+                    ImageUrl = "http://18.218.178.167/imagesemail/ferre1.jpg"
+                }
+            };
+
+            this.CLTiendas.ItemsSource = Alltiendas;
+
+        }
+
+        private void CLTiendas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.Any())
+            {
+                CLTiendas.SelectedItem = null;
+
+                DisplayAlert("Colletion view", "Selecion", "Cerrar");
+
+            }
+        }
     }
-}
 }
--- Menu-Tab-Maps/Menu-Tab-Maps/Ubicanos.xaml.cs
+++ Menu-Tab-Maps/Menu-Tab-Maps/Ubicanos.xaml.cs
@@ -5,16 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace Menu_Tab_Maps
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Ubicanos : ContentPage
-{
-    public Ubicanos()
+    public partial class Ubicanos : ContentPage
     {
-        InitializeComponent();
+        public Ubicanos()
+        {
+            InitializeComponent();
+            MostrarMapa(-2.155538, -79.8910639);
+        }
+
+
+        public void MostrarMapa(double Latitud, double Longitud)
+        {
+            base.OnAppearing();
+
+            MapaGeo.MoveToRegion(
+                MapSpan.FromCenterAndRadius
+                (
+                  new Position(Latitud, Longitud)  ,
+                  Distance.FromKilometers(0.5)
+                 )
+                );
+
+            var pos = new Position(Latitud, Longitud);
+
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = pos,
+                Label = "Curso de XAMRIN SIPECOM"
+            };
+
+            this.MapaGeo.Pins.Add(pin);
+
+        }
+
     }
-}
+
 }
