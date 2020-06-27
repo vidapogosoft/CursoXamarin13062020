f01cd5d24a8692ff063fe304374f35944e850c08
vidapogosoft <vidapogosoft@hotmail.com>
2020-06-27 13:52:59 -0500

    menu Master detail - tabbed
    menu detail
    tabbed

---
+++ Menu-Tab-Maps/Menu-Tab-Maps/Cuenta.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Menu_Tab_Maps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cuenta : TabbedPage
+    {
+        public Cuenta()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+
+
+}
---
+++ Menu-Tab-Maps/Menu-Tab-Maps/Home.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Menu_Tab_Maps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Home : ContentPage
+{
+    public Home()
+    {
+        InitializeComponent();
+    }
+}
+}
